02fd6e2a153c103debf3b000388982447d1ec150
Robert Sęk <robosek@tlen.pl>
2018-05-13 20:21:04 +0200

    Add topic

---
+++ topic/EmitLogTopic/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open RabbitMQ.Client
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.ExchangeDeclare("topic_logs", "topic")
+
+    let routingKey = argv.[0]
+    let message = String.Join(" ", argv.[1])
+    let body = Encoding.UTF8.GetBytes message
+
+    channel.BasicPublish("topic_logs", routingKey, null, body)
+    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+    Console.ReadKey() |> ignore
+    0
---
+++ topic/ReceiveLogTopic/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.ExchangeDeclare("topic_logs", "topic")
+    let queueName = channel.QueueDeclare() |> fun queue -> queue.QueueName
+
+    argv |> Array.iter(fun routingKey -> channel.QueueBind(queueName, "topic_logs" ,routingKey))
+
+    Console.WriteLine(" [*] Waiting for logs.");
+    
+    let consumer = new EventingBasicConsumer(channel)
+
+    let printReceivedMessage model (event:BasicDeliverEventArgs) =
+        let body = event.Body
+        let routingKey = event.RoutingKey
+        let message = Encoding.UTF8.GetString body
+        Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message)
+
+    let receiveMessageEventHandler = new EventHandler<BasicDeliverEventArgs>(printReceivedMessage)
+    consumer.Received.AddHandler(receiveMessageEventHandler)
+    channel.BasicConsume(queueName, true, consumer) |> ignore
+    Console.WriteLine "Press any key to exit"
+    Console.ReadLine() |> ignore
+    0
