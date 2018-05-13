c54e0a62d00e5f852ad83bdc5ce05646ba96b2d4
Robert Sęk <robert.sek@powel.com>
2018-05-13 18:22:47 +0200

    Add work-queue

---
+++ work-queues/NewTask/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open RabbitMQ.Client
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.QueueDeclare("task_queue", true, false, false, null) |> ignore
+    let properties = channel.CreateBasicProperties()
+    properties.Persistent <- true
+    
+    let message = String.Join(" ", argv)
+    let body = Encoding.UTF8.GetBytes message
+
+    channel.BasicPublish("", "task_queue", properties, body)
+    Console.WriteLine("Sent message: {0}", message)
+    Console.ReadKey() |> ignore
+    0
+
---
+++ work-queues/Worker/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Text
+open System.Threading
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.QueueDeclare("task_queue", true, false, false, null) |> ignore
+    channel.BasicQos(0u, 1us, false)
+
+    
+    let consumer = new EventingBasicConsumer(channel)
+
+    let printReceivedMessage model (event:BasicDeliverEventArgs) =
+        let body = event.Body
+        let message = Encoding.UTF8.GetString body
+        Console.WriteLine("Received message: {0}", message)
+
+        let dots = message.Split(".") |> Array.length
+        Thread.Sleep(dots * 1000)
+
+        Console.WriteLine("Work done")
+        channel.BasicAck(event.DeliveryTag, false)
+
+    let receiveMessageEventHandler = new EventHandler<BasicDeliverEventArgs>(printReceivedMessage)
+    consumer.Received.AddHandler(receiveMessageEventHandler)
+    channel.BasicConsume("task_queue", false, consumer) |> ignore
+    Console.WriteLine "Press any key to continue"
+    Console.ReadKey() |> ignore
+    0
