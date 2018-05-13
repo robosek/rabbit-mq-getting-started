d4a0d911fcc4aa1ed08f8d48c3033e0c7934ce87
Robert Sęk <robosek@tlen.pl>
2018-05-13 19:11:35 +0200

    Add pub-sub

---
+++ pub-sub/ReceiveLog/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.ExchangeDeclare("logs", "fanout")
+    let queueName = channel.QueueDeclare() |> fun queue -> queue.QueueName
+    channel.QueueBind(queueName, "logs" ,"")
+
+    Console.WriteLine(" [*] Waiting for logs.");
+    
+    let consumer = new EventingBasicConsumer(channel)
+
+    let printReceivedMessage model (event:BasicDeliverEventArgs) =
+        let body = event.Body
+        let message = Encoding.UTF8.GetString body
+        Console.WriteLine("Received logs: {0}", message)
+
+    let receiveMessageEventHandler = new EventHandler<BasicDeliverEventArgs>(printReceivedMessage)
+    consumer.Received.AddHandler(receiveMessageEventHandler)
+    channel.BasicConsume(queueName, true, consumer) |> ignore
+    Console.WriteLine "Press any key to continue"
+    Console.ReadKey() |> ignore
+    0
