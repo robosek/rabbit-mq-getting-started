de8844b13e019c0b5ce97baf180bbe22a2a048a0
Robert Sęk <robert.sek@powel.com>
2018-05-13 10:21:01 +0200

    hello-world example

---
+++ hello-world/Receive/Program.fs
@@ -0,0 +1,27 @@
+﻿
+open System
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.QueueDeclare("hello",false, false, false, null) |> ignore
+    
+    let consumer = new EventingBasicConsumer(channel)
+
+    let printReceivedMessage model (event:BasicDeliverEventArgs) =
+        let body = event.Body
+        let message = Encoding.UTF8.GetString body
+        Console.WriteLine("Received message: {0}", message)
+         
+    let receiveMessageEventHandler = new EventHandler<BasicDeliverEventArgs>(printReceivedMessage)
+    consumer.Received.AddHandler(receiveMessageEventHandler)
+    channel.BasicConsume("hello",true,consumer) |> ignore
+    Console.WriteLine "Press any key to continue"
+    Console.ReadKey() |> ignore
+    0
---
+++ hello-world/Send/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open RabbitMQ.Client
+open System.Text
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ConnectionFactory()
+    factory.HostName <- "localhost"
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    channel.QueueDeclare("hello",false, false, false, null) |> ignore
+    
+    let message = "hello world! 2"
+    let body = Encoding.UTF8.GetBytes message
+
+    channel.BasicPublish("","hello",null, body)
+    Console.WriteLine("Sent message: {0}", message)
+    Console.ReadKey() |> ignore
+    0
